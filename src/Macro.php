19a36a2e168b349a5e3450d588a500785cacff7b
domkrm <do_kraemer@gmx.de>
2020-09-06 07:58:13 +0200

    Add better support for macros and mixins (#1006)
    Co-authored-by: Dominik Krämer <do_kraemer@outlook.de>

--- src/Macro.php
+++ src/Macro.php
@@ -33,7 +33,26 @@ class Macro extends Method
      */
     protected function initPhpDoc($method)
     {
-        $this->phpdoc = new DocBlock($method);
+        $this->phpdoc = new DocBlock('/** */');
+
+        // Add macro parameters
+        foreach ($method->getParameters() as $parameter) {
+            $type = $parameter->hasType() ? $parameter->getType()->getName() : 'mixed';
+            $type .= $parameter->hasType() && $parameter->getType()->allowsNull() ? '|null' : '';
+
+            $name = $parameter->isVariadic() ? '...' : '';
+            $name .= '$' . $parameter->getName();
+
+            $this->phpdoc->appendTag(Tag::createInstance("@param {$type} {$name}"));
+        }
+
+        // Add macro return type
+        if ($method->hasReturnType()) {
+            $type = $method->getReturnType()->getName();
+            $type .= $method->getReturnType()->allowsNull() ? '|null' : '';
+
+            $this->phpdoc->appendTag(Tag::createInstance("@return {$type}"));
+        }
     }
 
     /**
